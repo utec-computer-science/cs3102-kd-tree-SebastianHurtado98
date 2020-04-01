e758b5961a8b3368c6c1ef0b0cf3c33fb3a29ce6
SebastianHurtado98 <sebastian.hurtado@utec.edu.pe>
2020-04-01 14:33:43 -0500

    templates, insert, search

--- kdtree.h
+++ kdtree.h
@@ -1,29 +1,85 @@
-#include "node.h"
+#include <iostream>
+#include <vector>
 
+template<typename Container>
+struct Node {
+    Container point =NULL;
+    Node* left =nullptr;
+    Node* right =nullptr;
+};
+
+
+template<typename Container>
 class KdTree {
     private:
-        Node *root;
+        Node<Container> *root;
         unsigned int degree;
 
     public:
+        // No creo que el degree vaya a cambiar de tipo
         KdTree(unsigned int degree) {
             this->degree = degree;
             root = nullptr;
         };
 
-        Node *search(int &elem) { 
-            
+        bool equalPoints(const Container &point1, const Container &point2) {
+            for (int i = 0; i < degree; i++) {
+                if (point1[i] != point2[i]) return false;
+            }
+            return true;
+        }
+
+        
+        bool search(Container &container) { 
+            bool found = false;
+            if(root != nullptr) {
+                int level = 1;
+                auto current = root;
+                auto dimension = (level-1)%degree;
+                while (!found) {
+                    if(equalPoints(current->point, container)) {
+                        found = true;
+                    } else {
+                        if(container[dimension] < current->point[dimension]) {
+                            if (current->left==nullptr) {
+                                return false;
+                            } else {
+                                current = current->left;
+                                level++;
+                            }
+                        }
+                        else {
+                            if (current->right==nullptr) {
+                                return false;
+                            } else {
+                                current = current->right;
+                                level++;
+                            }
+                        }
+                    }
+                    dimension = (level-1)%degree;
+                }
+
+            } else {
+                return false;
+            }
         } 
 
-        bool insert() {
+
+
+        bool insert(Container &container) {
+            auto newNode = new Node<Container>;
+            //esperemos que todos los containers tengan ese operador sobrecargado
+            //el container se borra después de igualarse?
+            newNode->point = container;
+
             if(root != nullptr) {
-                Node *newNode = new Node;
-                insertNewNode(degree, newNode);
                 bool notInserted = true;
                 int level = 1;
-                Node *current = root;
+                auto current = root;
+                auto dimension = (level-1)%degree;
                 while (notInserted) {
-                    if (newNode->point[(level-1)%degree] < current->point[(level-1)%degree]) {
+                    if (newNode->point[dimension] < current->point[dimension]) {
                         if (current->left==nullptr) {
                             current->left = newNode;
                             notInserted = false;
@@ -40,22 +96,26 @@ class KdTree {
                             level++;
                         }
                     }
+                    dimension = (level-1)%degree;
                 }
 
             } else {
-                root = new Node;
-                insertNewNode(degree,root);
+                root = new Node<Container>;
+                root->point = container;
             }
+        
         }
 
         void print() {
-            for (int i = 0; i < degree; i ++) {
-                std::cout << root->point[i];
+            auto node = root->right;
+            for (auto x: node->point) {
+                std::cout<<x;
             }
         }
 
         ~KdTree(){
             root = nullptr;
         };
+    
 };
 
--- main.cpp
+++ main.cpp
@@ -2,11 +2,26 @@
 #include "kdtree.h"
 #include <vector>
 
+typedef std::vector<int> container;
+
 int main() {
-    std::cout << "Hello World! \n";
-    KdTree TwoDTree(2);
-    TwoDTree.insert();
-    TwoDTree.print();
+    //al kd tree solo le pasas contenedores.
     
+    KdTree<container> tree(2);
+    
+    container myContainer;
+    myContainer.push_back(2);
+    myContainer.push_back(2);
+    
+    tree.insert(myContainer);
+
+    container myContainer2;
+    myContainer2.push_back(3);
+    myContainer2.push_back(3);
+    
+    tree.insert(myContainer2);
+
+    tree.print();
+
     return 0;
 }
